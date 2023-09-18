cedb77a41f7be13329463699ed4abe61f3465b04
evgeniy.satushev <evgeniysatushev@gmail.com>
2023-09-18 16:01:08 +0300

    add class App.java and Gradle build tool along with apache.commons-lang3 library

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.13.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
