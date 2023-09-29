6047450814d8f752a5c79e97be0e5e8dce4a2f73
evgeniy.satushev <evgeniysatushev@gmail.com>
2023-09-29 12:44:02 +0300

    Resend previous commit

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -10,7 +10,7 @@ public class Cli implements Engine {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
-        System.out.println("Hello " + userName + '!');
+        System.out.println("Hello, " + userName + '!');
         return userName;
     }
     @Override
